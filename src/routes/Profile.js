0db4b1c8236cca10463fce9dffce530213d760ad
MunSoJeong <sgh04056@naver.com>
2023-03-04 20:01:40 +0900

    🎨feat : Profile에서 내가 쓴 글 가져오기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,33 @@
-import { authService } from 'myBase'
+import { authService, db } from 'myBase'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { collection, query, where, orderBy, getDocs } from 'firebase/firestore'
 
-export default function Profile() {
+export default function Profile({ userObj }) {
   const navigate = useNavigate()
-  const onLogOut = () => {
+  const onClickLogOut = () => {
     authService.signOut()
     navigate('/')
   }
+
+  const getMySweets = async () => {
+    const q = query(
+      collection(db, 'sweets'),
+      where('creator', '==', userObj.uid),
+      orderBy('createdAt', 'desc'),
+    )
+    const queryResult = await getDocs(q)
+    queryResult.forEach((doc) => {
+      console.log(doc.id, ':', doc.data())
+    })
+  }
+  useEffect(() => {
+    getMySweets()
+  }, [])
+
   return (
     <>
-      <button onClick={onLogOut}>Log Out</button>
+      <button onClick={onClickLogOut}>Log Out</button>
     </>
   )
 }
